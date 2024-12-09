d2c3f396b86147dedf2747988a89ad65a583c9a8
caleb <lazerarms@gmail.com>
2024-12-09 11:57:09 -0600

    added the ability for the gun to rotate

---
+++ Perilous Plummet/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] private GameInput game_input;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        RotateGun();
+    }
+
+    private void RotateGun()
+    {
+        Vector3 mouse_position = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle_radians = Mathf.Atan2(mouse_position.y - transform.position.y, mouse_position.x - transform.position.x);
+        float angle_degreee = (180 / Mathf.PI) * angle_radians;
+
+        angle_degreee += FlipGun();
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle_degreee);
+        Debug.DrawLine(transform.position, mouse_position, Color.green, Time.deltaTime);
+    }
+
+    private float FlipGun()
+    {
+        if( game_input.GetHorizontalMovement() < 0)
+        {
+            return 180;
+        }
+
+        return 0;
+    }
+}
